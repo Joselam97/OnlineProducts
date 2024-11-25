6c818bc5a6b51e9f5c781a8edf76b6458d23bdbb
Jose Alvarez <jlam97@hotmail.es>
2024-11-25 13:18:43 -0600

    Implementacion de funcionalidad para cambio de lenguajes entre ingles y espaniol

--- src/main/java/org/jalvarez/jsf/ProducerResources.java
+++ src/main/java/org/jalvarez/jsf/ProducerResources.java
@@ -4,7 +4,11 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
 import org.jalvarez.jsf.qualifiers.CustomFacesContext;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 @ApplicationScoped
 public class ProducerResources {
@@ -16,5 +20,12 @@ public class ProducerResources {
             FacesContext facesContext = FacesContext.getCurrentInstance();
             facesContext.getExternalContext().getFlash().setKeepMessages(true);
             return facesContext;
+    }
+
+    @Produces
+    @Named("msg")
+    public ResourceBundle beanBundle() {
+        Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+        return ResourceBundle.getBundle("messages", locale);
     }
 }
