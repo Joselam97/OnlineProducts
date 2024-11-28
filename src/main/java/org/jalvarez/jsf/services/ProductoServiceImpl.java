4218c23ca55141b34904ea11c1ce31e032293a2e
Jose Alvarez <jlam97@hotmail.es>
2024-11-28 16:09:39 -0600

    Implementacion de seguridad JAAS y pagina JSF de form para Login y manejo de error en login

--- src/main/java/org/jalvarez/jsf/services/ProductoServiceImpl.java
+++ src/main/java/org/jalvarez/jsf/services/ProductoServiceImpl.java
@@ -1,5 +1,8 @@
 package org.jalvarez.jsf.services;
 
+import jakarta.annotation.security.DeclareRoles;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 import org.jalvarez.jsf.entities.Categoria;
@@ -11,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Stateless
+@DeclareRoles({"USER", "ADMIN"})
 public class ProductoServiceImpl implements ProductoService {
 
     @Inject
@@ -20,36 +24,43 @@ public class ProductoServiceImpl implements ProductoService {
     private CrudRepository<Categoria> categoriaRepository;
 
     @Override
+    @PermitAll
     public List<Producto> listar() {
         return repository.listar();
     }
 
     @Override
+    @RolesAllowed({"USER", "ADMIN"})
     public Optional<Producto> porId(Long id) {
             return Optional.ofNullable(repository.porId(id));
     }
 
     @Override
+    @RolesAllowed("ADMIN")
     public void guardar(Producto producto) {
         repository.guardar(producto);
     }
 
     @Override
+    @RolesAllowed("ADMIN")
     public void eliminar(Long id) {
         repository.eliminar(id);
     }
 
     @Override
+    @RolesAllowed({"USER", "ADMIN"})
     public List<Categoria> listarCategorias() {
         return categoriaRepository.listar();
     }
 
     @Override
+    @RolesAllowed({"USER", "ADMIN"})
     public Optional<Categoria> porIdCategoria(Long id) {
         return Optional.ofNullable(categoriaRepository.porId(id));
     }
 
     @Override
+    @RolesAllowed({"USER", "ADMIN"})
     public List<Producto> buscarPorNombre(String nombre) {
         return repository.buscarPorNombre(nombre);
     }
