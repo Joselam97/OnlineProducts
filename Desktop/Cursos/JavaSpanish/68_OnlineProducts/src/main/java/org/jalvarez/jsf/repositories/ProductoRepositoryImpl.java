0b3b2beea9e58300ab7d8aa2a116547cb13fdbda
Jose Alvarez <jlam97@hotmail.es>
2024-12-05 16:14:30 -0600

    Update .gitignore and clean untracked files

---
+++ Desktop/Cursos/JavaSpanish/68_OnlineProducts/src/main/java/org/jalvarez/jsf/repositories/ProductoRepositoryImpl.java
@@ -0,0 +1,50 @@
+package org.jalvarez.jsf.repositories;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import org.jalvarez.jsf.entities.Producto;
+
+import java.util.List;
+
+@RequestScoped
+public class ProductoRepositoryImpl implements ProductoRepository {
+
+    @Inject
+    private EntityManager em;
+
+    @Override
+    public List<Producto> listar() {
+        return em.createQuery("SELECT p FROM Producto p LEFT OUTER JOIN FETCH p.categoria", Producto.class).getResultList();
+    }
+
+    @Override
+    public Producto porId(Long id) {
+        //return em.find(Producto.class, id);
+        return em.createQuery("SELECT p FROM Producto p LEFT OUTER JOIN FETCH p.categoria WHERE p.id=:id", Producto.class)
+                .setParameter("id", id)
+                .getSingleResult();
+    }
+
+    @Override
+    public void guardar(Producto producto) {
+        if (producto.getId() != null && producto.getId() > 0) {
+            em.merge(producto);
+        } else {
+            em.persist(producto);
+        }
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        Producto producto = porId(id);
+        em.remove(producto);
+    }
+
+    @Override
+    public List<Producto> buscarPorNombre(String nombre) {
+        return em.createQuery("SELECT p FROM Producto p LEFT OUTER JOIN FETCH p.categoria WHERE p.nombre LIKE :nombre", Producto.class)
+                .setParameter("nombre", "%" + nombre + "%")
+                .getResultList();
+    }
+}
